62a9918fbc2badd32e9af4780bdbda0a996ded01
guninvalid <allenzfliu@gmail.com>
2019-03-14 20:51:04 -0700

    Create Figure8.java

---
+++ Karel/Chapter 2/Figure8 (2.9)/Figure8.java
@@ -0,0 +1,61 @@
+/**			
+ *
+ * @Author:
+ * Date: 
+ * Teacher: 
+ * Lab: HBot
+ *
+**/
+
+import kareltherobot.*;
+
+public class Figure8 extends UrRobot {
+	
+	public Figure8(int s, int a, Direction dir, int beeps) {
+	
+		super(s, a, dir, beeps);
+		
+	}
+	
+	public void task() {
+		
+		turnLeft(); //1
+		turnLeft(); 
+		turnLeft();
+		move();
+		turnLeft(); //5
+		move();
+		move();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		move();
+		turnLeft();
+		
+	}
+	
+}
