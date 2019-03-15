763f1ba03c94663989a24043be56ac0e13a466d6
guninvalid <allenzfliu@gmail.com>
2019-03-14 20:57:37 -0700

    Update Figure8.java

--- Karel/Chapter 2/Figure8 (2.9)/Figure8.java
+++ Karel/Chapter 2/Figure8 (2.9)/Figure8.java
@@ -19,13 +19,25 @@ public class Figure8 extends UrRobot {
 	
 	public void task() {
 		
+		//turns to move to the right side of the 8
 		turnLeft(); //1
 		turnLeft(); 
 		turnLeft();
 		move();
+		
+		//turns to move up to the middle
 		turnLeft(); //5
 		move();
 		move();
+		
+		//turns to move through the middle to the middle of the left side of the 8
+		turnLeft();
+		move();
+		move();
+		
+		//turns around the middle beeper
+		turnLeft();
+		turnLeft();
 		turnLeft();
 		move();
 		move();
@@ -39,16 +51,15 @@ public class Figure8 extends UrRobot {
 		turnLeft();
 		move();
 		move();
+		
+		//turns back to the middle of the 8
 		turnLeft();
 		turnLeft();
 		turnLeft();
 		move();
 		move();
-		turnLeft();
-		turnLeft();
-		turnLeft();
-		move();
-		move();
+		
+		//moves around back to starting point around beeper
 		turnLeft();
 		move();
 		move();
