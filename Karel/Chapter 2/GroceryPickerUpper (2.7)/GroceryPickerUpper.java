36e1f15e7e68ac4c61c0c5047014142cdc125ed6
guninvalid <allenzfliu@gmail.com>
2019-03-14 21:03:18 -0700

    Update GroceryPickerUpper.java

--- Karel/Chapter 2/GroceryPickerUpper (2.7)/GroceryPickerUpper.java
+++ Karel/Chapter 2/GroceryPickerUpper (2.7)/GroceryPickerUpper.java
@@ -13,36 +13,36 @@ public class GroceryPickerUpper implements Directions {
 	
 	public static void task(UrRobot karel)  {
 		
-		karel.turnLeft(); //1
-		karel.turnLeft();
-		karel.move();
-		karel.move();
-		karel.turnLeft(); //5
 		karel.turnLeft();
 		karel.turnLeft();
 		karel.move();
 		karel.move();
-		karel.move(); //10
+		karel.turnLeft();
+		karel.turnLeft();
+		karel.turnLeft();
+		karel.move();
+		karel.move();
+		karel.move();
 		karel.turnLeft();
 		karel.move();
 		karel.move();
 		karel.pickBeeper();
-		karel.turnLeft(); //15
-		karel.move();
-		karel.pickBeeper();
-		karel.move();
-		karel.turnLeft();
-		karel.move(); //20
-		karel.pickBeeper();
-		karel.turnLeft();
-		karel.turnLeft();
-		karel.turnLeft();
-		karel.move(); //25
 		karel.turnLeft();
 		karel.move();
 		karel.pickBeeper();
 		karel.move();
-		karel.move(); //30
+		karel.turnLeft();
+		karel.move();
+		karel.pickBeeper();
+		karel.turnLeft();
+		karel.turnLeft();
+		karel.turnLeft();
+		karel.move();
+		karel.turnLeft();
+		karel.move();
+		karel.pickBeeper();
+		karel.move();
+		karel.move();
 		
 	}
 	
