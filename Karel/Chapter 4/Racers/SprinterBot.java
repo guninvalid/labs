4965000205092a9ff2fbab8d14e7f1cd6765c7f2
guninvalid <allenzfliu@gmail.com>
2019-03-15 19:50:19 -0700

    Create SprinterBot.java

---
+++ Karel/Chapter 4/Racers/SprinterBot.java
@@ -0,0 +1,47 @@
+/**			
+ * 
+ * Author:
+ * Date: 
+ * Teacher: 
+ * Lab: Racers
+ * 
+**/
+
+import kareltherobot.*;
+
+public class SprinterBot extends AbstractRacerRobot {
+	
+	//Usual constructor
+	public SprinterBot(int st, int ave, Direction dir, int beeps) {
+		
+		super(st, ave, dir, beeps);
+		
+	}
+	
+	//Simply moves.
+	public void raceStride() {
+		
+		move();
+		
+	}
+	
+	//The following only left so asa to make sure they compile.
+	public void over() {
+		
+		
+		
+	}
+	
+	public void down() {
+		
+		
+		
+	}
+	
+	public void up() {
+		
+		
+		
+	}
+	
+}
