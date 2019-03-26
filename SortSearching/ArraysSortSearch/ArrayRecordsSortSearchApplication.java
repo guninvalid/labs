76217a499018d23e0eff6fa8e95ec2d2f01068ed
guninvalid <allenzfliu@gmail.com>
2019-03-26 09:44:06 -0700

    Update ArrayRecordsSortSearchApplication.java

--- SortSearching/ArraysSortSearch/ArrayRecordsSortSearchApplication.java
+++ SortSearching/ArraysSortSearch/ArrayRecordsSortSearchApplication.java
@@ -7,9 +7,7 @@
  * 
 **/
 	
-import java.util.Properties;
 import java.util.StringTokenizer;
-import java.util.Enumeration;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -21,18 +19,31 @@ public class ArrayRecordsSortSearchApplication {
 	
 	private static final String STUDENT_FILENAME = "students999.txt";
 	
+	//Main method.
 	public static void main(String args[]) {
 		
 		Student[] sortedStudents = null;
 		ClassPeriod p1 = new ClassPeriod("PERIOD 1");
 		readClass(p1);
+		
+		//The console reader. The APCSA teachers used chn.util for this but I opted 
+		//to use a buffered reader instead. Note that you can also use a buffered reader
+		//instead on the magpie lab you'll probably do later.
 		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		
+		//Apparently it is necessary for me to do this definition rather than 
+		//just leaving it as char choice;
 		char choice = '-';
 		
+		//Keeps showing menu again until user chooses 8 (8 is the quit option).
 		while (choice != '8') {
 			
 			showMenu();
 			
+			//Reads an option. If the option is longer than one character
+			//it will only read the first character. If the option is empty
+			//it will say that the human has not entered a choice and reset 
+			//the matrix.
 			try {
 				
 				choice = console.readLine().charAt(0);
@@ -46,19 +57,23 @@ public class ArrayRecordsSortSearchApplication {
 			
 			System.out.println();
 			
+			//The super mega useful switch that does everything.
 			switch (choice) {
 				
+				//Creates a file using CreateFiles.
 				case '0':
 						
 						createFile(console);
 						break;
 						
+				//Outputs to console the students straight from the class.
 				case '1':
 						
 						sortedStudents = p1.getStudents();
 						showStudents(sortedStudents);
 						break;
 						
+				//Reads a new class using human input for a different file.
 				case '2':
 						
 						System.out.println("Print name of file:");
@@ -73,40 +88,51 @@ public class ArrayRecordsSortSearchApplication {
 						}
 						break;
 						
+				//Sorts the current class using Arrays.sort.
 				case '3':
 						
 						sortedStudents = p1.getStudents();
 						sortViaArraysClass(sortedStudents);
 						break;
 						
+				//Sorts the current class using insertion sort as implemented
+				//by the human.
 				case '4':
 						
 						sortedStudents = p1.getStudents();
 						sortViaInsertion(sortedStudents);
 						break;
 						
+				//Sorts the current class using selection sort as implemented
+				//by the human.
 				case '5':
 						
 						sortedStudents = p1.getStudents();
 						sortViaSelection(sortedStudents);
 						break;
 						
+				//Sorts the current class using merge sort as implemented by
+				//the human.
 				case '6':
 						
 						sortedStudents = p1.getStudents();
 						sortViaMerge(sortedStudents);
 						break;
 						
+				//Shows the current state of students, sorted or otherwise.
 				case '7':
 						
 						showStudents(sortedStudents);
 						break;
 						
+				//Ends the matrix. Note that the work is not done here but in 
+				//the while loop from the start.
 				case '8':
 						
 						System.out.println("Byeeeeeeeeeeeeee");
 						break;
 						
+				//Continues if the choice is not a whole number from 0 to 8.
 				default:
 						
 						System.out.println("That's not a choice!");
@@ -116,6 +142,7 @@ public class ArrayRecordsSortSearchApplication {
 		}
 	}
 	
+	//Prints the menu.
 	public static void showMenu() {
 		
 		String nameOfUserOnThisMachine = System.getProperties().getProperty("user.name");
@@ -134,6 +161,7 @@ public class ArrayRecordsSortSearchApplication {
 		
 	}
 	
+	//Prints the students in the argument.
 	public static void showStudents(Student[] students) {
 		
 		for (int i = 0; i < students.length; i++) {
@@ -144,14 +172,18 @@ public class ArrayRecordsSortSearchApplication {
 		
 	}
 	
+	//Overloading statement.
 	public static void readClass(ClassPeriod p1) {
 		
 		readClass(p1, STUDENT_FILENAME);
 		
 	}
 	
+	//Reads to p1 the class in file filename.
 	public static void readClass(ClassPeriod p1, String filename) {
 		
+		//These test to make sure thre file exists. If it doesn't,
+		//it ends the loop and says so.
 		File tmp = new File(filename);
 		
 		if (!tmp.exists()) {
@@ -165,13 +197,15 @@ public class ArrayRecordsSortSearchApplication {
 		p1.clear();
 		BufferedReader in;
 		
+		//
 		try {
 			
-			in = new BufferedReader(new FileReader(filename));
+			in = new BufferedReader(new FileReader(tmp));
 			
-		} catch (FileNotFoundException e1) {
+		} catch (FileNotFoundException e) {
 			
-			System.out.println("I could not find your file!");
+			System.out.println("This is an actual bug. Code 1");
+			e.printStackTrace();
 			return;
 			
 		}
@@ -194,7 +228,9 @@ public class ArrayRecordsSortSearchApplication {
 					
 				} catch (IOException e) {
 					
+					System.out.println("This is an actual bug. Code 2");
 					e.printStackTrace();
+					return;
 					
 				}
 				
@@ -208,7 +244,7 @@ public class ArrayRecordsSortSearchApplication {
 		System.out.println("Choose choice 1 to display students.");
 		
 	}
-
+	
 	public static void sortViaInsertion(Student[] students) {
 		
 		long time = Sorter.sortViaInsertion(students);
@@ -224,7 +260,7 @@ public class ArrayRecordsSortSearchApplication {
 		System.out.println(" with the selection sort");
 		
 	}
-			
+	
 	public static void sortViaArraysClass(Student[] students) {
 		
 		long time = Sorter.sortViaJavaArraysClass(students);
@@ -241,6 +277,8 @@ public class ArrayRecordsSortSearchApplication {
 		
 	}
 	
+	//Uses console to read the users desired number of students and passes on 
+	//to CreateFiles.
 	public static void createFile(BufferedReader console) {
 		
 		System.out.println("How many students for the file?");
@@ -254,10 +292,11 @@ public class ArrayRecordsSortSearchApplication {
 		} catch (NumberFormatException e) {
 			
 			System.out.println("That's not a number!");
+			createFile(console);
 			
 		} catch (IOException e) {
 			
-			System.out.println("This is an actual bug.");
+			System.out.println("This is an actual bug. Code 0");
 			e.printStackTrace();
 			
 		}
