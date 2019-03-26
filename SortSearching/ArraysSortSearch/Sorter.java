c3cd5be0e65a5831650acc358520df58e915e227
guninvalid <allenzfliu@gmail.com>
2019-03-26 09:55:24 -0700

    Update Sorter.java

--- SortSearching/ArraysSortSearch/Sorter.java
+++ SortSearching/ArraysSortSearch/Sorter.java
@@ -1,74 +1,195 @@
 /**
-  * @author:
-  * 
-  * note:    Complete the compareTo and equals methods below.
-  */
-import java.text.DecimalFormat;
-public class Student implements Comparable<Student> {
+ * 
+ * Author:
+ * Date: 
+ * Teacher: 
+ * Lab: Racers
+ * 
+**/
+
+import java.util.Arrays;
+
+public final class Sorter {
 	
-	private String name;
-	private int id;
-	private double gpa;
-	
-	// This is complete
-	public Student(String name, int id, double gpa){
+	public static long sortViaJavaArraysClass(Comparable[] array){
 		
-		this.name = name;
-		this.id = id;
-		this.gpa = gpa;
+		StopWatch timer = new StopWatch();
+		timer.start();
+		
+		Arrays.sort(array);
+		
+		timer.stop();
+		return timer.getElapsedTime();
 		
 	}
 	
-	// The 3 accessors are complete
-	public String getName() { 
+	public static long sortViaInsertion(Comparable[] array){
 		
-		return name; 
+		StopWatch timer = new StopWatch();
+		timer.start();	
 		
-	}
-	
-	public int getId() { 
-		
-		return id; 
-	
-	}
-	
-	public double getGpa() { 
-		
-		return gpa; 
-		
-	}
-	
-	// This method is complete
-	public String toString() {
-		
-		String s = new String("");
-		DecimalFormat fmt = new DecimalFormat("0.00");
-		s = "gpa:" + fmt.format(getGpa()) + "\tname:" + getName() + "\tid:" + getId();
-		return s;
-		
-	}
-	
-	// Write so that the students come out in ascending(non-descending) order
-	// of GPAs. If the GPAs are equal, then the secondary sort criterion should 
-	// be based on descending(non-ascending) order of the names.
-	// Reminder: use the Double class to help determine "equality" for double primitives
-	//				due to the imprecise nature of real numbers
-	public int compareTo(Student otherStudent){
-		
-		int compared = new Double(this.gpa).compareTo(new Double(otherStudent.gpa));
-		if (compared != 0) {
+		for (int i = 1; i < array.length; i++) {
 			
-			return compared;
+			insert(array, i);
 			
 		}
 		
-		return -this.name.compareTo(otherStudent.name);
+		timer.stop();
+		return timer.getElapsedTime();
 		
 	}
 	
-	public boolean equals(Student otherObj){
+	public static long sortViaSelection(Comparable[] array){
 		
-		return this instanceof Student && otherObj instanceof Student && this.compareTo(otherObj) == 0;
+		StopWatch timer = new StopWatch();
+		timer.start();
+		
+		for (int i = 0; i < array.length - 1; i++) {
+			
+			swap(array, i, indexOfSmallest(array, i));
+			
+		}
+		
+		timer.stop();
+		return timer.getElapsedTime();
+	
+	}
+	
+	public static long sortViaMerge(Comparable[] array){
+		
+		StopWatch timer = new StopWatch();
+		timer.start();
+		
+		mergeSort(array);
+		
+		timer.stop();
+		return timer.getElapsedTime();
+		
+	}
+	
+	public static int indexOfSmallest(Comparable[] array, int initial) {
+		
+		int index = initial;
+		Comparable min = array[initial];
+		for (int j = initial + 1; j < array.length; j++) {
+			
+			if (min.compareTo(array[j]) > 0) {
+				
+				min = array[j];
+				index = j;
+				
+			}
+			
+		}
+		return index;
+		
+	}
+	
+	public static void swap(Object[] array, int initial, int finish) {
+		
+		Object tmp = array[initial];
+		array[initial] = array[finish];
+		array[finish] = tmp;
+		
+	}
+	
+	public static void shiftRight(Object[] array, int initial, int finish) {
+		
+		for (int i = finish; i > initial; i--) {
+			
+			array[i] = array[i - 1];
+			
+		}
+		
+	}
+	
+	public static void insert(Comparable[] array, int insertedObj) {
+		
+		Comparable insertion = array[insertedObj];
+		int insertTo;
+		
+		for (insertTo = insertedObj; insertTo > 0; insertTo--) {
+			
+			if (insertion.compareTo(array[insertTo - 1]) > 0) {
+				
+				break;
+				
+			}
+			
+		}
+		
+		insert(array, insertTo, insertedObj, insertion);
+		
+	}
+	
+	public static void insert(Object[] array, int initial, int finish, Object inserted) {
+		
+		shiftRight(array, initial, finish);
+		array[initial] = inserted;
+		
+	}
+	
+	public static void mergeSort(Comparable[] array, int left, int right) {
+		
+		if (left >= right) {
+			
+			return;
+			
+		}
+		
+		int mid = (left + right) / 2;
+		mergeSort(array, left, mid);
+		mergeSort(array, mid + 1, right);
+		
+		merge(array, left, mid, right);
+		
+	}
+	
+	public static void merge(Comparable[] array, int left, int mid, int right) {
+		
+		int l = left;
+		int r = mid + 1;
+		int i = left;
+		Comparable[] sort = new Comparable[array.length];
+		
+		while (l <= mid && r <= right) {
+			
+			if (array[l].compareTo(array[r]) < 0) {
+				
+				sort[i++] = array[l++];
+				
+			} else {
+				
+				sort[i++] = array[r++];
+				
+			}
+			
+		}
+		
+		while (l <= mid) {
+			
+			sort[i++] = array[l++];
+			
+		}
+		
+		while (r <= right) {
+			
+			sort[i++] = array[r++];
+			
+		}
+		
+		for (int j = left; j <= right; j++) {
+			
+			array[j] = sort[j];
+			
+		}
+		
+		
+	}
+	
+	public static void mergeSort(Comparable[] array) {
+		
+		mergeSort(array, 0, array.length - 1);
 		
 	}
 	
