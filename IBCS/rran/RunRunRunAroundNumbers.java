59d86651817c5c79cb70c01f70b30414403ba262
guninvalid <allenzfliu@gmail.com>
2020-09-29 06:50:49 -0700

    Create RunRunRunAroundNumbers.java

---
+++ IBCS/rran/RunRunRunAroundNumbers.java
@@ -0,0 +1,73 @@
+//package rran;
+
+public class RunRunRunAroundNumbers {
+	
+	public static boolean isRunRoundNumber(int num) {
+		
+		String s = String.valueOf(num);
+		final int N = s.length();
+		boolean[] sigma = new boolean[10];
+		Digit[] digits = new Digit[N];
+		
+		for (double i = 0; i < N; i++) {
+			
+			int n = (num / ((int) Math.pow(10.0, i))) % 10;
+			
+			if (sigma[n]) {
+				
+				return false;
+				
+			}
+			
+			digits[N - (int) i - 1] = new Digit(n);
+			sigma[n] = true;
+			
+		}
+		
+		int i = 0;
+		
+		for (double j = 0; j < N; j++) {
+			
+			if (digits[i].flag) {
+				
+				return false;
+				
+			}
+			
+			digits[i].flag = true;
+			i += digits[i].N;
+			i = i % N;
+			
+		}
+		
+		return i % N == 0;
+		
+	}
+	
+	public static int getNextRunAroundNumber(int seqNum) {
+		
+		while (!isRunRoundNumber(seqNum)) {
+			
+			seqNum++;
+			
+		}
+		
+		return seqNum;
+		
+	}
+	
+	private static class Digit {
+		
+		public final int N;
+		public boolean flag;
+		
+		public Digit(int pN) {
+			
+			this.N = pN;
+			this.flag = false;
+			
+		}
+		
+	}
+	
+}
